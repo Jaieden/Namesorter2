a8188393a54b74de292a0aea41482cdc55cdcfe0
jaieden girdler <this_is_bob12@hotmail.com>
2018-10-14 15:14:47 +1000

    AutoFac implementation

--- Namesorter2/Program.cs
+++ Namesorter2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -11,8 +12,8 @@ namespace Namesorter2
             FileData inputFile = new FileData(args[0]);
             FileData outputFile = new FileData("sorted-names-list.txt");
             string[] names = inputFile.FileContents;
-            Dictionary<string, IComparer<string>> sorters = new Dictionary<string, IComparer<string>>(){ {"-a", new AscendingNameSorter() }, { "-d", new DescendingNameSorter() } };
-            Array.Sort(names, sorters[args[1]]);
+            SorterGetter sortOrderGetter = new SorterGetter();
+            Array.Sort(names, sortOrderGetter.Resolve("args[1]"));
             Console.WriteLine(string.Join("\n",names));
             outputFile.FileContents = names;
         }
---
+++ Namesorter2/SorterGetter.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Autofac.Features.Indexed;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Namesorter2
+{
+    class SorterGetter
+    {
+
+        public IComparer<string> Resolve(string argFlag)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AscendingNameSorter>().Named<IComparer<string>>("-a");
+            builder.RegisterType<DescendingNameSorter>().Named<IComparer<string>>("-d");
+            var container = builder.Build();
+            IComparer<string> sorter = container.ResolveNamed<IComparer<string>>(argFlag);
+            return sorter;
+        }
+    }
+}
